f3558db15115adcfb7561890acdbadcfd6a3eb65
Ramiro Berrelleza <ramiro@okteto.com>
2023-04-06 22:07:24 -0700

    add success message

--- gcp/credentials/configure.sh
+++ gcp/credentials/configure.sh
@@ -2,3 +2,4 @@
 
 echo ${GCP_SERVICE_KEY} | base64 -d | gcloud auth activate-service-account --key-file=-
 gcloud --quiet config set project ${GCP_PROJECT_ID} 2>/dev/null
+echo "GCP CLI configured with project ${GCP_PROJECT_ID}"
